11a2b5fcdfa5f35ef8a02ab2744685308d3be149
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-02-27 16:53:35 -0600

    feat: add interceptors to send data with snake case and receive it with camel case

--- src/shared/api/heartSafeApi.ts
+++ src/shared/api/heartSafeApi.ts
@@ -1,7 +1,29 @@
 import axios from "axios";
+import snakecaseKeys from "snakecase-keys";
+import camelcaseKeys from "camelcase-keys";
 
 const baseURL: string = import.meta.env.VITE_API_BASE_URL;
 
 export const heartSafeApi = axios.create({
   baseURL,
 });
+
+heartSafeApi.interceptors.request.use(
+  function (config) {
+    config.data = snakecaseKeys(config.data, { deep: true });
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+heartSafeApi.interceptors.response.use(
+  function (response) {
+    response.data = camelcaseKeys(response.data, { deep: true });
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
